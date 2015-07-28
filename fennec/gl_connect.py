c7df2c6ac043981cc3e3a26dd6f3f71accc1fba6
mspah <mspah@zulily.com>
2015-07-27 21:57:12 -0700

    Started gl_connect

--- fennec/gl_connect.py
+++ fennec/gl_connect.py
@@ -3,5 +3,12 @@ from gitlab import *
 
 
 class GitlabConnect(object):
-    def __init__(self):
+    def __init__(self, token, url, connection=None):
+        self.token = token
+        self.url = url
+        self.connection = connection
+
+    def connect_to_api(self):
         pass
+
+
