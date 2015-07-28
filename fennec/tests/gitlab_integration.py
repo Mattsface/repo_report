9db203f3d218a89b342d4c450211d026e4d3fd2d
mspah <mspah@zulily.com>
2015-07-27 21:33:27 -0700

    Added config.ini to the gitignore

--- fennec/tests/gitlab_integration.py
+++ fennec/tests/gitlab_integration.py
@@ -1,7 +1,23 @@
 import unittest
 from fennec.fennec import Fennec
 from fennec.gl_connect import GitlabConnect
+import ConfigParser
 
 
 
 
+
+class TestFennec(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+class TestGitLabConnect(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
