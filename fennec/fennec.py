8bb71fb9be4d05622c2921a2b1fbc302f433a851
mspah <mspah@zulily.com>
2015-08-11 15:47:19 -0700

    I have no idea how to mock

--- fennec/fennec.py
+++ fennec/fennec.py
@@ -3,13 +3,13 @@ class Fennec(object):
     def __init__(self, gl):
         self.gl = gl
 
-    def group_look_up(self):
+    def groups(self):
         """
         return a list of group objects
         :return:
         """
         try:
-            self.gl.Group()
+            return self.gl.Group()
         except StandardError as e:
             raise StandardError(e)
 
--- fennec/tests/gitlab_integration.py
+++ fennec/tests/gitlab_integration.py
@@ -1,24 +1,46 @@
 import unittest
 from fennec.fennec import Fennec
-from mock import MagicMock
-from gitlab import *
+from mock import MagicMock, Mock, call
 
 
 class TestFennec(unittest.TestCase):
+
     def setUp(self):
         """
         Set up object mock
         """
-        gl = Gitlab()
-        gl.auth = MagicMock(None)
-        gl.Group = MagicMock()
+        class Gitlab(object):
+            _groupList = None
+
+            def auth(self):
+                return None
+
+            def Group(self):
+                return self._groupList
+
+        class Group(Gitlab):
+            _name = None
+
+            def name(self):
+                return self._name
+
+        testname = self.id().split(".")[2]
+
+        if testname == "test_group_collection":
+            group_list = ['group1', 'group2', 'group3', 'group4']
+            self.gl = Mock(spec=Gitlab)
+            self.gl.Group.return_value = [Mock(spec=Group, _new_name=group) for group in group_list]
+        else:
+            pass
 
     def tearDown(self):
-        pass
+        print "Tearing down tests"
 
-    def test_namespace_collection(self):
-        fen = Fennec()
-
+    def test_group_collection(self):
+        expected_list = ['group1', 'group2', 'group3', 'group4']
+        fen = Fennec(self.gl)
+        actually_list = [group.name for group in fen.groups()]
+        self.assertEqual(actually_list, expected_list, "Expected {}, but got {}".format(actually_list, expected_list))
 
     def test_failed_namespace_collection(self):
         pass
